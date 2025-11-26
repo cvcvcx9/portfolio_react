bb67154814aed845fd523902e7a40456a0f4d0ad
cvcvcx9 <cvcvcx9@gmail.com>
2025-11-27 07:47:47 +0900

    feat: 카피버튼 삭제

--- src/content/_meta.js
+++ src/content/_meta.js
@@ -1,10 +1,21 @@
 export default {
-  index: '🧑 메인페이지',
+  index: {
+    title:'🧑 메인페이지',
+    theme: {
+      copyPage:false
+    }
+  },
   keyProjects: {
     title:'🚀 대표 프로젝트',
+    theme: {
+      copyPage:false
+    }
     },
   projects: {
     title:'📜 전체 프로젝트',
+    theme: {
+      copyPage:false
+    }
   },
   
 }

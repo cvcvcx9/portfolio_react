27a8b34d236047b6fef56a3217e6839747224ce1
cvcvcx9 <cvcvcx9@gmail.com>
2025-11-26 18:27:57 +0900

    feat:깻냥,선택의왕,업로드

---
+++ src/content/projects/_meta.js
@@ -0,0 +1,14 @@
+export default {
+    projects: {
+        title:"프로젝트 목록"
+    },
+    kingOfChoice: {
+        title:"선택의 왕"
+    },
+    sonnuri: {
+        title:"손누리"
+    },
+    kkatnyang: {
+        title:"깻냥"
+    }    
+}
